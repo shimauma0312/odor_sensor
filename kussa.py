8eefe61cc808722d927fbccdc5cb68142cca7065
shimauma XL <86759249+shimauma0312@users.noreply.github.com>
2024-05-12 21:08:11 +0900

    Update kussa.py

--- kussa.py
+++ kussa.py
@@ -2,64 +2,69 @@
 import spidev
 import RPi.GPIO as GPIO
 import time
+import threading
 
 from pydub import AudioSegment
 from pydub.playback import play
 
-threshold = 600 #閾値を変えるときはここを変更
+def setup_spi():
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.max_speed_hz = 1000000
+    spi.bits_per_word = 8
+    return spi
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
-
-spi = spidev.SpiDev()
-
-spi.open(0,0)
-
-spi.max_speed_hz=1000000
-
-spi.bits_per_word=8
-
-dummy = 0xff
-start = 0x47
-sgl = 0x20
-
-ch0 = 0x00
-
-msbf = 0x08
-
-# read to mp3
-wav_file = AudioSegment.from_file("kuse1.wav", format="wav")
-
-
-def measure(ch):
-    ad = spi.xfer2( [ (start + sgl + ch + msbf), dummy ] )
+def measure(spi, ch):
+    start = 0x47
+    sgl = 0x20
+    msbf = 0x08
+    dummy = 0xff
+    ad = spi.xfer2([(start + sgl + ch + msbf), dummy])
     val = ((ad[0] & 0x03) << 8) + ad[1]
     return val
 
-try:
-    while 1:
-        time.sleep(0.237)
-              
-        GPIO.output(22,True)
-        time.sleep(0.003)
+def play_sound(wav_file):
+    play(wav_file)
 
-        ch0_val = measure(ch0)
-        Val = 1023 - ch0_val
-        time.sleep(0.002)
-        GPIO.output(22,False)
-        
-        GPIO.output(17,True)
-        time.sleep(0.008)
-        GPIO.output(17,False)
-        
-        print(Val)
+def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
 
-        if Val > threshold:
-            print("くっさ！！")
-            play(wav_file)
-           # pygame.mixer.music.play(0)
-except KeyboardInterrupt:
-    pass
+    spi = setup_spi()
 
-spi.close()
+    threshold = 600
+
+    wav_file = AudioSegment.from_file("kuse1.wav", format="wav")
+
+    try:
+        while True:
+            time.sleep(0.237)
+            
+            GPIO.output(22, True)
+            time.sleep(0.003)
+
+            ch0_val = measure(spi, 0)
+            Val = 1023 - ch0_val
+            time.sleep(0.002)
+            GPIO.output(22, False)
+            
+            GPIO.output(17, True)
+            time.sleep(0.008)
+            GPIO.output(17, False)
+            
+            print(Val)
+
+            if Val > threshold:
+                print("くっさ！！")
+                sound_thread = threading.Thread(target=play_sound, args=(wav_file,))
+                sound_thread.start()
+                
+    except KeyboardInterrupt:
+        pass
+
+    spi.close()
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()
